4af6f75fef35f384d5b3f5a16814de82607419eb
kazuya <k.uesato0521@gmail.com>
2021-05-04 17:28:20 +0900

    インデント整形

--- src/main/java/com/example/demo/presentation/user/UserController.java
+++ src/main/java/com/example/demo/presentation/user/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.Objects;
 import java.util.Optional;
 
@@ -36,10 +37,10 @@ public class UserController {
                                              @RequestParam("email") String email,
                                              @RequestParam(name = "tel", required = false) String tel,
                                              UriComponentsBuilder uriBuilder) {
-        try{
-                Optional<MultipartFile> profileImage = Optional.empty();
-                if(Objects.nonNull(profileImageData)) {
-                    profileImage = Optional.of(profileImageData);
+        try {
+            Optional<MultipartFile> profileImage = Optional.empty();
+            if (Objects.nonNull(profileImageData)) {
+                profileImage = Optional.of(profileImageData);
             }
 
             userApplicationService.create(userId, firstName, lastName, screenName, profileImage, email, tel);
